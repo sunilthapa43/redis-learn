1f193254b9a52306668513d35d87fdc119b1c99f
Sunil Thapa <tsunil359@gmail.com>
2022-07-05 13:20:28 +0545

    serialized items

--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,10 @@
 import type { CreateItemAttrs } from '$services/types';
+import { attr } from 'svelte/internal';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs) => {
+    return{
+        ...attrs,
+        createdAt: attrs.createdAt.toMillis(),
+        endingAt: attrs.endingAt.toMillis()
+    }
+};
